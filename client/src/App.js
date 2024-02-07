3e50e2176e16cee52c47a7ed4383b09f30470606
Chenyuan Gu <guchenyuan@outlook.com>
2024-02-07 15:17:56 +0800

    finish forecast

--- client/src/App.js
+++ client/src/App.js
@@ -8,4 +8,4 @@ const App = () => (
       </BackgroundImage>
 );
 
-export default App;
+export default App; 
--- client/src/apis/getForecast.js
+++ client/src/apis/getForecast.js
@@ -1,6 +1,5 @@
-//TODO:add your own api key in env
 const getForecast = ({CITY}) =>
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API_KEY}&units=metric`)
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=10c517980602db404824afd4d676faa3&units=metric`)
         .then((res) => res.json() )
 
 export default getForecast;
